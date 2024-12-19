fa3c0924319f6f0969f9c8eef32a1766e7fd82e8
CalvinGaiennie <calvingaiennie@gmail.com>
2024-12-19 16:15:37 -0600

    added to dos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,11 @@ function About() {
       <div className="center textDiv">
         <h1>About Fetch More Kennels</h1>
       </div>
-      <p>Text Text Text</p>
-      <p>Text Text Text</p>
-      <p>Text Text Text</p>
+      <p>heres the basic layout so far</p>
+      <p>with some better photos it might look ok</p>
+      <p>I need to </p>
+      <p>add more contact info</p>
+      <p>add a cms so Andy can update the site</p>
     </div>
   );
 }
